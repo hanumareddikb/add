bc436f7a3ba12c764ea823d4b0a5124df8592568
hanumareddi.kb <hanumareddi.kb@dkv.global>
2024-06-24 16:10:41 +0530

    changes

--- add.py
+++ add.py
@@ -1,8 +1,14 @@
 class Calculate:
 
-    def add(a, b):
-        return a + b
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
 
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
-print("Sum : ",Calculate.add(num1, num2))
+
+obj = Calculate(num1, num2)
+print("sum : ",obj.add())
